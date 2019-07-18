091f77b8f5b496cd1d3892b1512d90655c1d1395
gTomasik <grzesiek315@gmail.com>
2019-07-18 15:09:43 +0200

    initializing config

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the bitch',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Bruhh, so cold',
+    iconName: 'snowflake'
+  }
+}
+
+const getSeason = (lat,month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter'
+    }else{
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName}  = seasonConfig[season]
+
+
+    return (
+      <div>
+        <i className = {`massive ${iconName} icon`} />
+        <h1>{text}</h1>
+        <i className = {`massive ${iconName} icon`} />
+      </div>
+    )
+}
+
+export default SeasonDisplay
